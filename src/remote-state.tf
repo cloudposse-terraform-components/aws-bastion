559f25403cd57c3b7444162b642e20d1d2c84e42
Dan Miller <miller0daniel@gmail.com>
2025-09-12 19:15:08 -0400

    fix: Destruction (#40)
    * Update module version and add VPC defaults
    
    * Update module version and add VPC defaults

--- src/remote-state.tf
+++ src/remote-state.tf
@@ -4,5 +4,11 @@ module "vpc" {
 
   component = var.vpc_component_name
 
+  defaults = {
+    vpc_id             = ""
+    public_subnet_ids  = []
+    private_subnet_ids = []
+  }
+
   context = module.this.context
 }
